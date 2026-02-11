a025324e5e4ce5c4836e43f54a7ac7fb532a5533
imredshadow <jianturalbaacol@gmail.com>
2026-02-11 13:41:12 +0000

    67

--- CanAttend.java
+++ CanAttend.java
@@ -2,12 +2,23 @@ import java.util.ArrayList;
 
 public class CanAttend {
 
-	//precondition: 
-	//postcondition: 
+	//precondition: Meetings has more than null and each meeting has a start and end time
+	//postcondition: If no meetings overlap, it returns true, if any meetings conflicts it returns false
 	public static boolean canAttend(ArrayList<MeetingInterval> meetings) {
 		//your implementation here
-		System.out.println(meetings);
-		return false; //replace me with actual logic please!
+		if (meetings.size() <= 1) {
+			return true;
+		}
+		for (int i = 0; i < meetings.size(); i++) {
+			for (int j = i + 1; j < meetings.size(); j++) {
+				MeetingInterval m1 = meetings.get(i);
+                MeetingInterval m2 = meetings.get(j);
+				if (m1.getStart() < m2.getEnd() && m2.getStart() < m1.getEnd()) {
+                    return false;
+                }
+			}
+		}
+		return true;
 	}
 	public static void main(String[] args) {
 		ArrayList<MeetingInterval> meet = new ArrayList<MeetingInterval>();
